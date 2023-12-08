bf7cc0ba7cacb21ca7a80cbc2a9cfb5d052675f3
tr3s1n <tresa.lukas@gmail.com>
2023-12-08 15:42:44 +0100

    Add custom exceptions for remaining entities

---
+++ src/main/java/com/tresin/cvproj/handmade_shop/exception/CartNotFoundException.java
@@ -0,0 +1,12 @@
+package com.tresin.cvproj.handmade_shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CartNotFoundException extends RuntimeException {
+
+    public CartNotFoundException(String message) {
+        super(message);
+    }
+}
